e4ffe1b158692f1b829360c9f13b8877081ec8d2
PoojithaBethu2525 <poojithabethu@gmail.com>
2024-12-10 21:46:06 +0530

    initial commit

---
+++ test_lambda.py
@@ -0,0 +1,8 @@
+import json
+def lambda_handler(event, context):
+    print("Lambda from my code")
+    
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello Lambda! From my code')
+    }
